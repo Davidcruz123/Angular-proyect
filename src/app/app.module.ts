512954c33f4ad87d4f9c26c30a0341b670bd67db
David <davidc@vivogaming.com>
2023-06-25 12:51:54 -0600

    first local commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { RecipeDetailComponent } from './component/recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './component/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './component/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './component/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './component/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesComponent } from './component/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
