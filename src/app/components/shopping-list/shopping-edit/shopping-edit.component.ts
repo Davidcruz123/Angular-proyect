2335e97e43987a6ba193277d42c5fd07d2d2ff97
David <davidc@vivogaming.com>
2023-07-09 10:37:57 -0600

    using template driven approach validation

--- src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Ingredient } from '../../../shared/models/ingredient.model';
 import { ShoppingListService } from 'src/app/shared/services/shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
 })
 export class ShoppingEditComponent {
+  @ViewChild("formRefence") form:NgForm;
   constructor(private shoppingListService : ShoppingListService) {
 
   }
-  public AddElement(nameInput:HTMLInputElement,amountInput:HTMLInputElement):void{
-      this.shoppingListService.addIngredient(new Ingredient(nameInput.value,+amountInput.value));
-      nameInput.value = "";
-      amountInput.value= "0";
+  public AddElement():void{
+      const formValues = this.form.value
+      this.shoppingListService.addIngredient(new Ingredient(formValues.productName,+formValues.amount));
+      this.form.reset()
   }
 
 }
