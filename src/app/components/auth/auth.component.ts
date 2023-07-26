dc1816f81b81dd94b2f58160800ab74ad09341ab
David <davidc@vivogaming.com>
2023-07-25 22:10:32 -0600

    Switching between auth modes

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -5,5 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './auth.component.html',
 })
 export class AuthComponent {
+  isloginMode = true;
 
+  public onSwitchMode() {
+    this.isloginMode= !this.isloginMode;
+  }
 }
